0d7164b27eb3fbe96be138eb661fe6816a81b87f
Tim Morgan <tim@timmorgan.org>
2008-12-19 10:36:04 -0600

    remove src extension

--- build.rb
+++ build.rb
@@ -8,7 +8,7 @@ content = File.read(README).split(/^h2./).first.strip + "\n\n"
 
 LIBS.each do |lib|
   content << "h2. #{NAMESPACE}.#{lib}\n\n"
-  code = File.read(File.join('src', lib + '.src.js'))
+  code = File.read(File.join('src', lib + '.js'))
   code.scan(/(^\/\/.+?)(mini\.[a-z0-9_]+\.[a-z0-9_]+)=function\(([a-z0-9_,]*)\)/m).each do |doc, name, args|
     content << "@#{name}(#{args})@\n"
     content << doc.gsub(/^\/\/\s*/, '').sub(/\n+$/, '') + "\n\n"
